cb3c84510dde0c326df83609528c6a149dccbf3b
y4wl <yothpi@gmail.com>
2017-01-31 10:36:20 +0100

    inite

---
+++ Assets/WaveControler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveControler : MonoBehaviour {
+
+	public float height;
+	public float time;
+
+	// Use this for initialization
+	void Start () {
+		iTween.MoveBy(this.gameObject, iTween.Hash("y", height, "time", time, "looptype", "pingpong", "easetype", iTween.EaseType.easeInOutSine));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
